b409debc42e09927f17cf8105ff2726bc880197d
Ruby <c19890611@gmail.com>
2017-09-25 20:32:19 -0500

    commom e-commerce webiste review form

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+let button = document.querySelector(".button")
+let show = document.querySelector(".show")
+let review = document.querySelector(".review")
+
+button.addEventListener('click', function(){
+    console.log("button click")
+    alert("Thanks for your review!!")
+})
+
+
+// review.submit();
